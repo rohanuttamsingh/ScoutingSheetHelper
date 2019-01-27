b1574ed2eabaf514537b97bc69b88d99b36237eb
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2019-01-27 14:39:59 -0500

    Began schedule structure Can get schedule for individual color alliance Working on auto-populating document with schedule

--- spreadsheet.py
+++ spreadsheet.py
@@ -7,35 +7,39 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 class Spreadsheet:
+
+    # comment out one of these depending on which spreadsheet being used
+    url = 'https://docs.google.com/spreadsheets/d/1WhExw_ReHnyPQYXl0p-kT6jYXpZW5w8-cq2ffK7niOs' # 'Deep Space Scouting Machine'
+    # url = 'https://docs.google.com/spreadsheets/d/1lOTML4TgNqv5OKUJU32keWu62__T9cFT3IL52kmPbKk' # 'Bethesda Week 2 Scouting Machine' 
     
     # google sheets credentials setup
     scope = ['https://spreadsheets.google.com/feeds']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret_gsheets.json', scope)
     client = gspread.authorize(creds)
 
-    # tabs of google sheet
-    sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1WhExw_ReHnyPQYXl0p-kT6jYXpZW5w8-cq2ffK7niOs')
+    # google sheets document
+    sheet = client.open_by_url(url)
+    
+    # individual worksheets of google sheets document
     key_worksheet = sheet.worksheet('Key')
     teams_worksheet = sheet.worksheet('Teams')
     sample_team_worksheet = sheet.worksheet('Sample Team')
+    schedule_worksheet = sheet.worksheet('Schedule')
+
+    # setting event key to value in A1 of Key worksheet
+    event_key = key_worksheet.cell(1, 1).value
 
     # tba setup
     tba_session = requests.Session()
     BASE_URL = 'https://www.thebluealliance.com/api/v3'
-
-    # event key of intended competition
-    event_key = ''
 
     # tba credentials setup
     with open('client_secret_tba.json') as json_file:
         data = json.load(json_file)
         tba_auth_key = data['tba_auth_key']
 
-    # setting event key to value in spreadsheet
-    event_key = key_worksheet.cell(1, 1).value
-
     def __init__(self):
-        """All requests will have authentication key in header"""
+        """All TBA requests will have authentication key in header"""
         self.tba_session.headers.update({'X-TBA-Auth-Key': self.tba_auth_key})
 
     def get_teams_from_event(self, event):
@@ -101,7 +105,7 @@ class Spreadsheet:
                     sleep(1.1)
                 elif col != '':
                     copy_to.update_cell(i, j, col)
-                    sleep(1.1) # Quota is 100 requests per 100 s, this does 100 requests per 110 s
+                    sleep(1.01) # Quota is 100 requests per 100 s, this does 100 requests per 101 s
                 j += 1
             i += 1
             j = 1
@@ -111,28 +115,62 @@ class Spreadsheet:
         for team in self.teams_worksheet.col_values(1):
             self.copy_sheet(self.get_sample_sheet(), self.sheet.worksheet(team), team)
 
-    def get_blue_schedule(self, event):
+    def get_color_schedule(self, event, color):
+        """Returns match schedule of specified color alliance in list
+        
+        event: event key of intended competition (e.g. 2018vahay)
+        color: color of desired alliance schedule (e.g. red or blue)
+        """
         # event schedules get updated to elims event schedules once elims are reached
         # only elims schedule accessible in finished events
-        blue_schedule = []
+        schedule = []
         event_list = self.tba_session.get(self.BASE_URL + '/event/%s/matches/simple' % event).json() # list of dicts
         for match in event_list:
-            blue_schedule.append(match['alliances']['blue'])
-        for quals_alliances in schedule:
-            for i in range(len(quals_alliances)):
-                quals_alliances[i] = quals_alliances[i][3:]
-                # Trims 'frc' from beginning of every team number
-        return blue_schedule
+            schedule.append(match['alliances'][color]['team_keys'])
+        for alliance in schedule:
+            for i in range(len(alliance)):
+                alliance[i] = alliance[i][3:]
+                # trims 'frc' from beginning of every team number
+        return schedule
+
+    def fill_schedule(self, event):
+        blue_schedule = self.get_color_schedule(event, 'blue')
+        red_schedule = self.get_color_schedule(event, 'red')
+        
+        # # updates num_matches to the correct number of matches and fill column 1 of spreadsheet with match number
+        num_matches = 1
+        for match in range(len(blue_schedule)):
+            self.schedule_worksheet.update_cell(match + 1, 1, match + 1)
+            num_matches += 1
+            sleep(1.01)
+
+        # while not exit:
+        #     self.schedule_worksheet.update_cell(match, 1, match)
+        #     sleep(1.01)
+        #     for i in range(3):
+        #         if not blue_schedule[match][i]:
+        #             exit = True
+        #             break
+        #         self.schedule_worksheet.update_cell(match, i + 2, blue_schedule[match][i])
+        #         sleep(1.01)
+        #     for j in range(3):
+        #         if not red_schedule[match][i]:
+        #             exit = True
+        #             break
+        #         self.schedule_worksheet.update_cell(match, i + 5, red_schedule[match][i])
+        #         sleep(1.01)
+        #     match += 1
 
     def main(self):
         # self.fill_teams(self.teams_worksheet, self.event_key)
         # self.create_team_sheets()
         # self.delete_team_sheets()
         # print(self.get_sample_sheet())
-        # self.copy_sheet(self.get_sample_sheet(), self.sheet.worksheet('1137')) # testing on single sheet
+        # self.copy_sheet(self.get_sample_sheet(), self.sheet.worksheet('1111'), 1111) # testing on single sheet
         # print(len(self.get_sample_sheet()))
         # self.copy_sample_to_team_sheets()
-        print(self.get_blue_schedule(self.event_key))
+        # print(self.get_color_schedule(self.event_key, 'red'))
+        self.fill_schedule(self.event_key)
 
 
 if __name__ == '__main__':
